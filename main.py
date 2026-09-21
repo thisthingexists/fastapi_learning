dbf15e5d1c40a8bfaa68aab46b21b110c5291f39
CybeRShashank <114229044+CybeRShashank@users.noreply.github.com>
2026-09-21 17:49:52 +0530

    updated learning stack till video 2

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, Request , HTTPException, status
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles 
+
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"),name ="static")
+templates = Jinja2Templates(directory="templates")
+
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Corey Schafer",
+        "title": "FastAPI is Awesome",
+        "content": "This framework is really easy to use and super fast.",
+        "date_posted": "April 20, 2025",
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "title": "Python is Great for Web Development",
+        "content": "Python is a great language for web development, and FastAPI makes it even better.",
+        "date_posted": "April 21, 2025",
+    },
+]
+@app.get("/api/posts")
+def get_posts():
+    return posts
+
+@app.get("/api/post/{post_id}")
+def get_post(post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+
+
+
+@app.get("/", include_in_schema = False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"})
